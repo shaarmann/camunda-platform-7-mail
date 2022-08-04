6d4450935516f7987a5b9a1db31962e8e9fb5d9d
Jonathan Lukas <jonathan.lukas@camunda.com>
2022-08-04 11:02:47 +0200

    Field injection is evil!

--- extension-run/src/main/java/org/camunda/bpm/extension/mail/run/MailConnectorPlugin.java
+++ extension-run/src/main/java/org/camunda/bpm/extension/mail/run/MailConnectorPlugin.java
@@ -3,31 +3,35 @@ package org.camunda.bpm.extension.mail.run;
 import org.camunda.bpm.engine.impl.cfg.AbstractProcessEnginePlugin;
 import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
 import org.camunda.bpm.extension.mail.MailConnectors;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.camunda.bpm.extension.mail.config.MailConfiguration;
 import org.springframework.stereotype.Component;
 
 import java.util.logging.Logger;
 
 @Component
 public class MailConnectorPlugin extends AbstractProcessEnginePlugin {
-  @Autowired
-  protected MailConfigurationProvider mailConfigurationProvider;
+  private final Logger LOGGER = Logger.getLogger(this
+      .getClass()
+      .getName());
 
-  private final Logger LOGGER = Logger.getLogger(this.getClass().getName());
+  private final MailConfiguration mailConfiguration;
+
+  public MailConnectorPlugin(MailConfiguration mailConfiguration) {this.mailConfiguration = mailConfiguration;}
 
   @Override
   public void preInit(ProcessEngineConfigurationImpl processEngineConfiguration) {
-    LOGGER.info("Configuring Mail Connectors");
-    MailConnectors.deleteMails().setConfiguration(mailConfigurationProvider);
-    MailConnectors.pollMails().setConfiguration(mailConfigurationProvider);
-    MailConnectors.sendMail().setConfiguration(mailConfigurationProvider);
-  }
-
-  public MailConfigurationProvider getMailConfigurationProvider() {
-    return mailConfigurationProvider;
-  }
-
-  public void setMailConfigurationProvider(MailConfigurationProvider mailConfiguration) {
-    this.mailConfigurationProvider = mailConfiguration;
+    LOGGER.info("Configuring Mail Connectors with properties: ");
+    mailConfiguration
+        .getProperties()
+        .forEach((o, o2) -> LOGGER.info(o + " = " + o2));
+    MailConnectors
+        .deleteMails()
+        .setConfiguration(mailConfiguration);
+    MailConnectors
+        .pollMails()
+        .setConfiguration(mailConfiguration);
+    MailConnectors
+        .sendMail()
+        .setConfiguration(mailConfiguration);
   }
 }
